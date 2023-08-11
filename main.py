db77439348503735c0e163b5d939de2a62c12427
xyspg <kangjm2005@hotmail.com>
2023-08-11 15:06:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://telegram-analyzer.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
